19b3fa2faf238fd9761142543de234278f7fc664
matiassalles99 <matiassalles99@gmail.com>
2022-05-17 01:13:21 -0300

    test exercise 4

--- run_tests.sh
+++ run_tests.sh
@@ -2,7 +2,14 @@ echo "Running all tests..."
 
 ./ejercicio_1/scripts/run_tests.sh
 if [ $? -ne 0 ]; then
-    echo "Error running tests for ejercicio_1"
+    echo "Error running tests for exercise 1"
     exit 1
 fi
 
+
+./ejercicio_4/scripts/run_tests.sh
+
+if [ $? -ne 0 ]; then
+    echo "Error running tests for exercise 4"
+    exit 1
+fi
